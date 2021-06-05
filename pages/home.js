00ec2e80c29e3801a2e480edd9a1c0749aad8f57
VedantWankhade <vedantwankhade17@gmail.com>
2021-06-06 01:18:50 +0530

    Route Links to favorites and myNotes from home page

--- pages/home.js
+++ pages/home.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
         <div>
             <h1>JANTA</h1>
             <p>This is the home page.</p>
+            {/* list of links */}
+            <ul>
+                <li>
+                    <Link to='/myNotes'>My Notes</Link>
+                </li>
+                <li>
+                    <Link to='/favorites'>Favorites</Link>
+                </li>
+            </ul>
         </div>
     );
 }
