edf4a8ee616d36596969949414e3f71aa3e22a68
VedantWankhade <vedantwankhade17@gmail.com>
2021-06-06 02:04:06 +0530

    Create styled button component

--- pages/home.js
+++ pages/home.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-import Header from "../components/Header";
-import Navigation from "../components/Navigation";
+import Button from "../components/Button";
 
 const Home = () => {
     return (
         <div>
             <p>This is the home page.</p>
+            <Button>Click Me!</Button>
         </div>
     );
 }
