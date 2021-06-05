e7ba727a44cd1afc398e76d89c1d9b5c209ee7ae
VedantWankhade <vedantwankhade17@gmail.com>
2021-06-06 02:09:40 +0530

    Basic global styles

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 
+import GlobalStyle from "./components/GlobalStyle";
 import Pages from "./pages";
 
 const App = () => {
     return (
         <div>
+            <GlobalStyle />
             <Pages />
         </div>
     );
