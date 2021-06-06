2c1cc790e8e425c103f371536a6c60d38a259805
VedantWankhade <vedantwankhade17@gmail.com>
2021-06-06 18:22:23 +0530

    Added pagination (fetch and append more notes) on clicking button

--- pages/home.js
+++ pages/home.js
@@ -1,9 +1,8 @@
 import React from "react";
 import { useQuery, gql } from "@apollo/client";
-// to render markdown
-import ReactMarkdown from "react-markdown";
 
 import NoteFeed from "../components/NoteFeed";
+import Button from "../components/Button";
 
 const GET_NOTES = gql`
     query NoteFeed($cursor: String) {
@@ -32,8 +31,42 @@ const Home = () => {
     if (loading) return <p>Loading....</p>;
     // if error, display error message
     if (error) return <p>Sorry and error occured!</p>
-    // if successfull return the data in UI
-    return <NoteFeed notes={data.noteFeed.notes} />;
+    // if successful return the data in UI
+    return (
+        <>
+            <NoteFeed notes={data.noteFeed.notes} />
+            {/* Only show this button if hasNextPage is true */}
+            {
+                // This executes data.noteFeed.hasNextPage first and if it is true then renders Button
+                // Refer: https://reactjs.org/docs/conditional-rendering.html
+                data.noteFeed.hasNextPage
+                &&
+                <Button
+                    onClick={() =>
+                        fetchMore({
+                            variables: {
+                                cursor: data.noteFeed.cursor
+                            },
+                            updateQuery: (previousResult, { fetchMoreResult }) => {
+                                return {
+                                    noteFeed: {
+                                        cursor: fetchMoreResult.noteFeed.cursor,
+                                        hasNextPage: fetchMoreResult.noteFeed.hasNextPage,
+                                        // append new notes to old ones
+                                        notes: [
+                                            ...previousResult.noteFeed.notes,
+                                            ...fetchMoreResult.noteFeed.notes
+                                        ],
+                                        _typename: 'noteFeed'
+                                    }
+                                }
+                            }
+                        }
+                    )}
+                >Load More</Button>
+            }
+        </>
+    );
 }
 
 export default Home;
