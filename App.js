5f532cde7a8069f08eaeb7be3227fad6ad3a92ab
VedantWankhade <vedantwankhade17@gmail.com>
2021-06-06 19:42:50 +0530

    Configure apollo client to authorization:JWT with each GQL request

--- App.js
+++ App.js
@@ -1,19 +1,38 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import {
+    ApolloClient,
+    ApolloProvider,
+    InMemoryCache,
+    createHttpLink
+} from "@apollo/client";
+import { setContext } from "apollo-link-context";
 
 import GlobalStyle from "./components/GlobalStyle";
 import Pages from "./pages";
 
-// We don't need to import and configure dotenv as parcel automatically does it
 // configure our API URI and cache
+// We don't need to import and configure dotenv as parcel automatically does it
 const API_URI = process.env.API_URI;
 const cache = new InMemoryCache();
+const httpLink = createHttpLink({ uri: API_URI });
+
+// check for a token and return the headers to the context
+const authLink = setContext((_, { headers }) => {
+    return {
+        headers: {
+            ...headers,
+            authorization: localStorage.getItem('token') || ''
+        }
+    }
+});
 
 // configure apollo client
 const client = new ApolloClient({
-    uri: API_URI,
+    // now this link has header which has authorization whose value is user JWT token
+    link: authLink.concat(httpLink),
     cache,
+    resolvers: {},
     connectToDevTools: true
 });
 
