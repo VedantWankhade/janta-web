f099e2b7b3b61a7c3a81581be8ed0280d87c57f0
VedantWankhade <vedantwankhade17@gmail.com>
2021-06-07 00:47:36 +0530

    Use UserForm in signin and signup

--- pages/signin.js
+++ pages/signin.js
@@ -1,14 +1,36 @@
 import React, {useEffect} from "react";
+import { useMutation, useApolloClient, gql } from "@apollo/client";
+
+import UserForm from "../components/UserForm";
+
+const SIGNIN_USER = gql`
+    mutation signIn($email: String, $password: String!) {
+        signIn(email: $email, password: $password)
+    }
+`;
 
 const SignIn = props => {
     useEffect(() => {
         document.title = 'Sign In - JANTA';
     });
 
+    const client = useApolloClient();
+    const [signIn, { loading, error }] = useMutation(SIGNIN_USER, { onCompleted: data => {
+            localStorage.setItem('token', data.signIn);
+            client.cache.writeData({ data: { isLoggedIn: true } });
+            props.history.push('/');
+        } });
+
     return (
-        <div>
-            <p>Sign in page</p>
-        </div>
+        <React.Fragment>
+            <UserForm action={signIn} formType='signin' />
+            {
+                loading && <p>Loading...</p>
+            }
+            {
+                error && <p>Sorry, error occured while signing in!</p>
+            }
+        </React.Fragment>
     );
 }
 
--- pages/signup.js
+++ pages/signup.js
@@ -1,29 +1,7 @@
 import React, { useEffect, useState } from "react";
-import styled from "styled-components";
 import { useMutation, useApolloClient, gql } from "@apollo/client";
 
-import Button from "../components/Button";
-
-const Wrapper = styled.div`
-  border: 1px solid #f5f4f0;
-  max-width: 500px;
-  padding: 1em;
-  margin: 0 auto;
-  box-shadow: 3px 3px 5px 6px #ccc;
-`;
-
-const Form = styled.form`
-  label,
-  input {
-    display: block;
-    line-height: 2em;
-  }
-  
-  input {
-    width: 100%;
-    margin-bottom: 1em;
-  }
-`;
+import UserForm from "../components/UserForm";
 
 const SIGNUP_USER = gql`
     mutation signUp($email: String!, $username: String!, $password: String!) {
@@ -35,17 +13,6 @@ const SignUp = props => {
     useEffect(() => {
         document.title = 'Sign Up - JANTA';
     });
-
-    // set default state of the form
-    const [values, setValues] = useState();
-
-    // update the state when a user types in form
-    const onChange = event => {
-        setValues({
-            ...values,
-            [event.target.name]: event.target.value
-        });
-    }
 
     // apollo client hook to store status of user logged in or not
     // This client is the same client we initialized in App.js
@@ -61,48 +28,17 @@ const SignUp = props => {
     }})
 
     return (
-        <Wrapper>
-            <h2>Sign Up</h2>
-            <Form
-                onSubmit={event => {
-                        event.preventDefault();
-                        signUp({
-                           variables: {
-                               ...values
-                           }
-                        });
-                        event.target.reset();
-                }}
-            >
-                <label htmlFor="username">Username:</label>
-                <input
-                    type="text"
-                    id="username"
-                    name="username"
-                    placeholder="Username"
-                    onChange={onChange}
-                />
-                <label htmlFor="email">Email:</label>
-                <input
-                    required
-                    type="email"
-                    id="email"
-                    name="email"
-                    placeholder="Email"
-                    onChange={onChange}
-                />
-                <label htmlFor="password">Password:</label>
-                <input
-                    required
-                    type="password"
-                    id="password"
-                    name="password"
-                    placeholder="Password"
-                    onChange={onChange}
-                />
-                <Button type="submit">Submit</Button>
-            </Form>
-        </Wrapper>
+        <>
+            <UserForm action={signUp} formType='signup' />
+            {/* If data is loading, display loading message */}
+            {
+                loading && <p>Loading...</p>
+            }
+            {/* if error, show error message */}
+            {
+                error && <p>Sorry, an error accured signin up!</p>
+            }
+        </>
     );
 }
 
