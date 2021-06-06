e9ca12dfb01ac207442de91065a8c9002cb49a58
VedantWankhade <vedantwankhade17@gmail.com>
2021-06-06 16:21:19 +0530

    Display notes

--- App.js
+++ App.js
@@ -1,15 +1,28 @@
 import React from "react";
 import ReactDOM from 'react-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 import GlobalStyle from "./components/GlobalStyle";
 import Pages from "./pages";
 
+// We don't need to import and configure dotenv as parcel automatically does it
+// configure our API URI and cache
+const API_URI = process.env.API_URI;
+const cache = new InMemoryCache();
+
+// configure apollo client
+const client = new ApolloClient({
+    uri: API_URI,
+    cache,
+    connectToDevTools: true
+});
+
 const App = () => {
     return (
-        <>
+        <ApolloProvider client={client}>
             <GlobalStyle />
             <Pages />
-        </>
+        </ApolloProvider>
     );
 }
 
--- pages/home.js
+++ pages/home.js
@@ -1,12 +1,52 @@
 import React from "react";
+import { useQuery, gql } from "@apollo/client";
+// to render markdown
+import ReactMarkdown from "react-markdown";
 
-import Button from "../components/Button";
+const GET_NOTES = gql`
+    query NoteFeed($cursor: String) {
+        noteFeed(cursor: $cursor) {
+            cursor
+            hasNextPage
+            notes {
+                id
+                createdAt
+                content
+                favoriteCount
+                author {
+                    username
+                    id
+                    avatar
+                }
+            }
+        }
+    }
+`;
 
 const Home = () => {
+    // query hook
+    const { data, loading, error, fetchMore } = useQuery(GET_NOTES);
+    // if the data is loading, display loading message
+    if (loading) return <p>Loading....</p>;
+    // if error, display error message
+    if (error) return <p>Sorry and error occured!</p>
+    // if successfull return the data in UI
     return (
         <div>
-            <p>This is the home page.</p>
-            <Button>Click Me!</Button>
+            {
+                data.noteFeed.notes.map(note =>
+                    <article key={note.id}>
+                        <img src={note.author.avatar} alt={`${note.author.username} avatar`} height='50px' />
+                        {'  '}
+                        {note.author.username} {note.createdAt} {note.favoriteCount}
+                        {'  '}
+                        <ReactMarkdown>
+                            {note.content}
+                        </ReactMarkdown>
+                        <hr/>
+                        {'  '}
+                    </article>)
+            }
         </div>
     );
 }
