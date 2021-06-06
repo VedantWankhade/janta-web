0dbf67be8721ceb2bc3466f993afe98d0afda7e3
VedantWankhade <vedantwankhade17@gmail.com>
2021-06-07 01:05:34 +0530

    Make favorites and myNotes restricted pages

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import { useQuery, gql } from "@apollo/client";
 
 import Layout from "../components/Layout";
-
 import Home from "./home";
 import MyNotes from "./myNotes";
 import Favorites from "./favorites";
@@ -10,19 +10,47 @@ import NotePage from "./note";
 import SignUp from "./signup";
 import SignIn from "./signin";
 
+const IS_LOGGED_IN = gql`
+    {
+        isLoggedIn @client
+    }
+`;
+
 const Pages = () => {
     return (
         <Router>
             {/* Wrap routes within the 'Layout' component */}
             <Layout>
                 <Route exact path='/' component={Home} />
-                <Route path='/myNotes' component={MyNotes} />
-                <Route path='/favorites' component={Favorites} />
+                <PrivateRoute path='/myNotes' component={MyNotes} />
+                <PrivateRoute path='/favorites' component={Favorites} />
                 <Route path='/note/:id' component={NotePage} />
                 <Route path='/signup' component={SignUp} />
                 <Route path='/signin' component={SignIn} />
             </Layout>
         </Router>
+    );
+}
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+    const { loading, error, data } = useQuery(IS_LOGGED_IN);
+    if (loading) return <p>Loading...</p>
+    if (error) return <p>Sorry, an error occured!</p>
+    // else redirtect to sign in page
+    return (
+        <Route
+            {...rest}
+            render={props => data.isLoggedIn === true ? (
+                <Component {...props} />
+            ) : (
+                <Redirect to={{
+                    pathname: '/signin',
+                    state: { from: props.location }
+                    }}
+                />
+            )
+            }
+        />
     );
 }
 
