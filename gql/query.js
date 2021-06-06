9352d477ac25cdc867c8a7a66a229de49267e350
VedantWankhade <vedantwankhade17@gmail.com>
2021-06-07 01:59:38 +0530

    Seperate filr for queries

---
+++ gql/query.js
@@ -0,0 +1,45 @@
+import { gql } from '@apollo/client';
+
+const GET_NOTES = gql`
+    query NoteFeed($cursor: String) {
+        noteFeed(cursor: $cursor) {
+            cursor
+            hasNextPage
+            notes {
+                id
+                createdAt
+                content
+                favoriteCount
+                author {
+                    username
+                    id
+                    avatar
+                }
+            }
+        }
+    }
+`;
+
+const GET_NOTE = gql`
+    query note($id: ID!) {
+        note(id: $id) {
+            id
+            createdAt
+            content
+            favoriteCount
+            author {
+                username
+                id
+                avatar
+            }
+        }
+    }
+`;
+
+const IS_LOGGED_IN = gql`
+    {
+        isLoggedIn @client
+    }
+`;
+
+export { GET_NOTE, GET_NOTES, IS_LOGGED_IN };
