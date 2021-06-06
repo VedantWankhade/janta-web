2e0b5fa0c874f7954d6cef2af4329561bbc687de
VedantWankhade <vedantwankhade17@gmail.com>
2021-06-06 17:41:12 +0530

    Style NoteFeed

--- components/NoteFeed.js
+++ components/NoteFeed.js
@@ -1,13 +1,23 @@
 import React from "react";
+import styled from 'styled-components';
+
 import Note from "./Note";
+
+const NoteWrapper = styled.div`
+  max-width: 800px;
+  margin: 0 auto;
+  margin-bottom: 2em;
+  padding: 1.5em;
+  box-shadow: 3px 3px 5px 6px #ccc;
+`;
 
 const NoteFeed = ({ notes }) => {
     return (
         <div>
-            {notes.map(note => <
-                div key={note.id}>
+            {notes.map(note =>
+                <NoteWrapper key={note.id}>
                     <Note note={note} />
-                </div>)
+                </NoteWrapper>)
             }
         </div>
     );
